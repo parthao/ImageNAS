9ed98c64e450a00a56370e4b5eab35d40d304d91
VishwaTechnologiesPvtLtd <151992843+VishwaTechnologiesPvtLtd@users.noreply.github.com>
2024-07-15 22:20:35 +0530

    ddd

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.get("/images/:filename", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 8088;
+const PORT = process.env.PORT || 808;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
