95bce6ef33b30d9fddf7006cbc235b8a421cd331
VishwaTechnologiesPvtLtd <151992843+VishwaTechnologiesPvtLtd@users.noreply.github.com>
2024-07-19 07:56:05 +0530

    made changes

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ app.use(cors(corsOptions));
 // Resolve IMAGE_DIRECTORY to an absolute path
 const IMAGE_DIRECTORY = path.resolve(__dirname, "/home/drago/Images/");
 
+// Resolve IMAGE_DIRECTORY to an absolute path
+const VIDEO_DIRECTORY = path.resolve(__dirname, "/home/drago/Videos/");
+
 // Route to list all images
 app.get("/images", (req, res) => {
   fs.readdir(IMAGE_DIRECTORY, (err, files) => {
@@ -36,9 +39,37 @@ app.get("/images", (req, res) => {
   });
 });
 
+// Route to list all videos
+app.get("/videos", (req, res) => {
+  fs.readdir(VIDEO_DIRECTORY, (err, files) => {
+    if (err) {
+      return res.status(500).send("Unable to scan directory");
+    }
+
+    const imageFiles = files.filter((file) => file.endsWith(".mp4"));
+    const imageLinks = imageFiles.map(
+      (file) => `${req.protocol}://${req.get("host")}/videos/${file}`
+    );
+    res.json(imageLinks);
+  });
+});
+
 // Route to serve an individual image
 app.get("/images/:filename", (req, res) => {
   const filePath = path.join(IMAGE_DIRECTORY, req.params.filename);
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      console.error("Error sending file:", err);
+      res.status(err.status).send("File not found");
+    } else {
+      console.log("Sent:", filePath);
+    }
+  });
+});
+
+// Route to serve an individual videos
+app.get("/videos/:filename", (req, res) => {
+  const filePath = path.join(VIDEO_DIRECTORY, req.params.filename);
   res.sendFile(filePath, (err) => {
     if (err) {
       console.error("Error sending file:", err);
